ae93279adaca0770c8580a36aacecc4ae360ff0d
donnafarris <dlfarris97@gmail.com>
2024-06-14 09:26:06 -0600

    Added 40 threshold

--- sql_queries/emp_name.sql
+++ sql_queries/emp_name.sql
@@ -1,9 +1,15 @@
+
 select
-	person.firstname || ' ' || 	person.lastname as employeename,
-	employee.jobtitle
+	person.firstname || ' ' || person.lastname as Employee_Name,
+	employee.jobtitle as Job_Title,
+	employee.vacationhours as Vacation_Hours,
+	emailaddress.emailaddress as Email_Address
 from
 	humanresources.employee
 join person.person
 on
 	employee.businessentityid = person.businessentityid
-limit 5;
+join person.emailaddress
+  on
+	person.emailaddress.businessentityid = person.businessentityid
+where employee.vacationhours > 39;
