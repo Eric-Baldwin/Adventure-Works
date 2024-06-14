f7367e2715ce6f9458444354a42548302a72fabc
eric-baldwin <erictbaldwinjr@gmail.com>
2024-06-14 10:55:50 -0600

    added subcategory

---
+++ sql_queries/product_subcategory.sql
@@ -0,0 +1,17 @@
+SELECT
+    production.productcategory.name AS category_name,
+    production.productsubcategory.name AS subcategory_name,
+    MIN(production.product.listprice) AS lowest_price,
+    MAX(production.product.listprice) AS highest_price,
+    MAX(production.product.listprice) - MIN(production.product.listprice) AS price_difference,
+    COUNT(production.product.productid) AS product_count
+FROM
+    production.product
+JOIN
+    production.productsubcategory ON production.product.productsubcategoryid = production.productsubcategory.productsubcategoryid
+JOIN
+    production.productcategory ON production.productsubcategory.productcategoryid = production.productcategory.productcategoryid
+GROUP BY
+    production.productcategory.name, production.productsubcategory.name
+ORDER BY
+    production.productcategory.name, production.productsubcategory.name;
