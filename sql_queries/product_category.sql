e44e1ae209a7065d3be4d8633f1a2bf2ba09b474
eric-baldwin <erictbaldwinjr@gmail.com>
2024-06-14 10:47:21 -0600

    Added product_category.sql

---
+++ sql_queries/product_category.sql
@@ -0,0 +1,16 @@
+SELECT
+    production.productcategory.name AS category_name,
+    MIN(production.product.listprice) AS lowest_price,
+    MAX(production.product.listprice) AS highest_price,
+    MAX(production.product.listprice) - MIN(production.product.listprice) AS price_difference,
+    COUNT(production.product.productid) AS product_count
+FROM
+    production.product
+JOIN
+    production.productsubcategory ON production.product.productsubcategoryid = production.productsubcategory.productsubcategoryid
+JOIN
+    production.productcategory ON production.productsubcategory.productcategoryid = production.productcategory.productcategoryid
+GROUP BY
+    production.productcategory.name
+ORDER BY
+    production.productcategory.name;
